2e9fec2a9a9c94400ad534493e8abf0dfc7df4a6
Marko Tasic <mtasic85@gmail.com>
2024-07-09 14:01:26 +0200

    cibuildwheels build process

--- scripts/build_all.py
+++ scripts/build_all.py
@@ -1,11 +1,17 @@
 import subprocess
 
+import build_clean
+import build_libllama_cli
+import build_llama_cli_cffi
 
 def build(*args, **kwargs):
     print(f'build {args = }, {kwargs = }')
-    subprocess.run(['poetry', 'run', 'build-clean'])
-    subprocess.run(['poetry', 'run', 'build-libllama-cli'])
-    subprocess.run(['poetry', 'run', 'build-llama-cli-cffi'])
+    # subprocess.run(['poetry', 'run', 'build-clean'])
+    # subprocess.run(['poetry', 'run', 'build-libllama-cli'])
+    # subprocess.run(['poetry', 'run', 'build-llama-cli-cffi'])
+    build_clean.clean()
+    build_libllama_cli.build()
+    build_llama_cli_cffi.build()
 
 
 if __name__ == '__main__':
