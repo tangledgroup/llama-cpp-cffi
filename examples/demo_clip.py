cf5080d0a04917ba0a57d00a0510c70f8708a930
Marko Tasic <mtasic85@gmail.com>
2024-12-04 19:59:08 +0100

    clip models: demo

---
+++ examples/demo_clip.py
@@ -0,0 +1,69 @@
+from llama import get_config, Model, Options
+
+from llama import (
+    backend_init,
+    backend_free,
+    model_init,
+    model_free,
+    context_init,
+    context_free,
+    sampler_init,
+    sampler_free,
+    clip_init_context,
+    clip_free_context,
+    clip_completions,
+)
+
+from demo_models import models
+
+
+# model_id: str = 'openbmb/MiniCPM-Llama3-V-2_5'
+model_id: str = 'openbmb/MiniCPM-V-2_6'
+# model_id: str = 'vikhyatk/moondream2'
+# model_id: str = 'liuhaotian/llava-v1.6-mistral-7b'
+
+model: Model = models[model_id]
+config = get_config(model.creator_hf_repo)
+
+options = Options(
+    model=model,
+    ctx_size=getattr(config, 'max_position_embeddings', 2048),
+    predict=-2,
+    temp=0.7,
+    top_p=0.8,
+    top_k=100,
+    # prompt='What is in the image?',
+    prompt='What is in the image? Extract (OCR) all text from page as markdown.',
+    # prompt='Extract (OCR) all text from page as markdown.',
+    image='examples/llama-4.png',
+    gpu_layers=99,
+)
+
+backend_init()
+
+_model = model_init(options)
+print(f'{_model=}')
+
+_ctx = context_init(_model, options)
+print(f'{_ctx=}')
+
+_sampler = sampler_init(options)
+print(f'{_sampler=}')
+
+_clip_ctx = clip_init_context(options)
+print(f'{_clip_ctx=}')
+
+input('Press any key to generate')
+
+for token in clip_completions(_model, _ctx, _sampler, _clip_ctx, options):
+    print(token, end='', flush=True)
+
+print()
+
+input('Press any key to exit')
+
+clip_free_context(_clip_ctx)
+sampler_free(_sampler)
+context_free(_ctx)
+model_free(_model)
+backend_free()
